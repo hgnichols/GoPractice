a5db72d36ca68c68fdf9911830b42fbde390b45b
Hunter Nichols <hnichols@sentryone.com>
2021-04-13 16:15:40 -0400

    Added ability to pass in file path and log level as command line parameters

--- PracticeConsoleApp/main.go
+++ PracticeConsoleApp/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"strings"
 )
 
 func main() {
-	data, err := ioutil.ReadFile("myapp.log")
+	path := flag.String("path", "myapp.log", "The path to the log that should be analyzed")
+	level := flag.String("level", "ERROR", "Log level to search for. Options are Debug, Info, and Critical")
+
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(*path)
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
@@ -16,7 +22,7 @@ func main() {
 	dataAsString := string(data)
 	eachLine := strings.Split(dataAsString, "\n")
 	for _, line := range eachLine {
-		if strings.Contains(line, "ERROR") {
+		if strings.Contains(line, *level) {
 			fmt.Println(line)
 		}
 	}
